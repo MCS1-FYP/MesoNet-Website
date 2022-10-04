608855f3128d61f6d0ddb317bc8e75a24af150cc
CheeMinHao <cheemh3456@gmail.com>
2022-10-04 21:50:51 +0800

    added image upload function

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+import io
+from tkinter import Image
 import streamlit as st
 import pandas as pd
 
-st.title("Image DeepFake Detector")
+def load_image():
+    uploaded_file = st.file_uploader(label='Pick an image to predict')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+
+def mesonet_predict():
+    pass
+
+
+def main():
+    st.title("Image DeepFake Detector")
+    load_image()
+
+if __name__ == '__main__':
+    main()
